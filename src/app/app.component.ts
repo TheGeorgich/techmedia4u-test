522c28bd1f405734dd6cced984f1307cf876afa9
TheGeorgich <vitaly.struk@eleken.co>
2019-09-12 11:06:49 +0300

    feat: create transactions list component, remove unused styles and markups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'techmedia4u-test';
+  appTitle = 'Angular budget App'
 }
