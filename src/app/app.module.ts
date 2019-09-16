953897b3c45dce828b740750696908cda6bf9645
TheGeorgich <vitaly.struk@eleken.co>
2019-09-16 10:39:54 +0300

    feat: create transaction-list, add-transaction and transaction-details components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TransactionsComponent } from './transactions/transactions.component';
-import { TransactionComponent } from './transaction/transaction.component';
+import { TransactionsListComponent } from './transactions-list/transactions-list.component';
 import { FormsModule } from '@angular/forms';
+import { BudgetFormComponent } from './budget-form/budget-form.component';
+import { AddTransactionComponent } from './add-transaction/add-transaction.component';
+import { TransactionDetailsComponent } from './transaction-details/transaction-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TransactionsComponent,
-    TransactionComponent,
+    TransactionsListComponent,
+    BudgetFormComponent,
+    AddTransactionComponent,
+    TransactionDetailsComponent,
   ],
   imports: [
     BrowserModule,
